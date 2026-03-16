3cf80611714fce945cdc901d21b53935c2705faa
prasanthmogalapalli2007-ops <prasanthmogalapalli2007@gmail.com>
2026-03-16 10:25:48 +0530

    UC3

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,54 +1,43 @@
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * =============================================================
- * CLASS – RoomInventory
- * =============================================================
- * Manages centralized room availability using HashMap
- */
-
-class RoomInventory {
-
-    private Map<String, Integer> roomAvailability;
-
-    // Initialize inventory
-    public RoomInventory() {
-
-        roomAvailability = new HashMap<>();
-
-        roomAvailability.put("Single Room", 5);
-        roomAvailability.put("Double Room", 3);
-        roomAvailability.put("Suite Room", 2);
-    }
-
-    // Return room availability
-    public Map<String, Integer> getRoomAvailability() {
-        return roomAvailability;
-    }
-
-    // Update availability
-    public void updateAvailability(String roomType, int count) {
-        roomAvailability.put(roomType, count);
-    }
-}
-
-
-/**
- * =============================================================
- * MAIN CLASS – BookMyStayApp
- * =============================================================
- */
-
 public class BookMyStayApp {
 
+    // RoomInventory class inside the same file
+    static class RoomInventory {
+
+        private Map<String, Integer> roomAvailability;
+
+        // initialize inventory
+        public RoomInventory() {
+
+            roomAvailability = new HashMap<>();
+
+            roomAvailability.put("Single Room", 5);
+            roomAvailability.put("Double Room", 3);
+            roomAvailability.put("Suite Room", 2);
+        }
+
+        // return room availability
+        public Map<String, Integer> getRoomAvailability() {
+            return roomAvailability;
+        }
+
+        // update availability
+        public void updateAvailability(String roomType, int count) {
+            roomAvailability.put(roomType, count);
+        }
+    }
+
+    // main method
     public static void main(String[] args) {
 
         RoomInventory inventory = new RoomInventory();
 
-        System.out.println("Current Room Inventory\n");
+        System.out.println("Room Inventory:\n");
 
-        for (Map.Entry<String, Integer> entry : inventory.getRoomAvailability().entrySet()) {
+        for (Map.Entry<String, Integer> entry :
+                inventory.getRoomAvailability().entrySet()) {
 
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
@@ -57,7 +46,8 @@ public class BookMyStayApp {
 
         inventory.updateAvailability("Single Room", 4);
 
-        for (Map.Entry<String, Integer> entry : inventory.getRoomAvailability().entrySet()) {
+        for (Map.Entry<String, Integer> entry :
+                inventory.getRoomAvailability().entrySet()) {
 
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
